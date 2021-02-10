cf36b3c46b6b60e20205edebbf5152a330abe15b
deni_k <deniz.kesmez@rwth-aachen.de>
2021-02-10 03:45:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+export interface SelectModel {
+  value: number;
+  viewValue: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'umrechner';
+  public lengthMeasurements: SelectModel[] = [
+    {value: 6, viewValue: 'km'},
+    {value: 5, viewValue: 'm'},
+    {value: 4, viewValue: 'cm'},
+    {value: 3, viewValue: 'mm'},
+    {value: 2, viewValue: 'µm'},
+    {value: 1, viewValue: 'nm'}
+  ];
+  public volumeMeasurements: SelectModel[] = [
+    {value: 1, viewValue: 'm³'},
+    {value: 2, viewValue: 'cm³'},
+    {value: 3, viewValue: 'dm³'},
+    {value: 4, viewValue: 'm³'}
+    ];
+  public umrechnen(model1: SelectModel, model2: SelectModel, value: number): number {
+    if (model1.value < model2.value) {
+      return value * 10 ^ (model2.value - model1.value);
+    } else if (model1.value > model2.value){
+      return value * 10 ^ (model2.value - model1.value);
+    } else {
+      return value;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MatInputModule,
+    MatSelectModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
